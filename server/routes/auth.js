8eed5772451e0b9d654ab61acd070eaf9e820aba
Anvar <anvarmusa12@gmail.com>
2023-08-21 12:27:02 +0200

    log out

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
 const { signup_post, login_post } = require("../controllers/AuthController");
+const { logout } = require("../controllers/LogoutController");
 const router = Router();
 
 router.post("/signup", signup_post);
 
 router.post("/login", login_post);
+
+router.post("/logout", logout);
 
 module.exports = router;
