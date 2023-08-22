7843ed6e4c0167a6771beddc1af22ce37a0e5e37
Anvar <anvarmusa12@gmail.com>
2023-08-22 14:36:45 +0200

    button detection

--- client/src/components/SingleTitleCard.jsx
+++ client/src/components/SingleTitleCard.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import { HeartIcon, SaveIcon, StarIcon } from "./svgIcons";
 
 const SingleTitleCard = ({ title, mediaType }) => {
+    // References
+    const favRef = useRef(null);
+    const saveRef = useRef(null);
+
+    const onClick = (e) => {
+        e.preventDefault();
+
+        // Add to favorites
+        if (e.target === favRef.current || favRef.current.contains(e.target)) {
+            console.log("cool fav");
+        }
+
+        // Add to seen
+        if (
+            e.target === saveRef.current ||
+            saveRef.current.contains(e.target)
+        ) {
+            console.log("cool save");
+        }
+    };
     return (
-        <Link to={`/title/${mediaType}/${title?.id}`} className="">
-            <img src={title?.poster_path} alt={title?.title}  className="rounded-lg"/>
+        <Link
+            to={`/title/${mediaType}/${title?.id}`}
+            className=""
+            onClick={onClick}
+        >
+            <img
+                src={title?.poster_path}
+                alt={title?.title}
+                className="rounded-lg"
+            />
             <div className="mt-4">
                 <h3>{title?.title}</h3>
                 <div className="flex justify-between">
@@ -14,10 +42,10 @@ const SingleTitleCard = ({ title, mediaType }) => {
                         <StarIcon />
                     </div>
                     <div className="flex gap-1">
-                        <button className="fav_btn">
+                        <button className="fav_btn" ref={favRef}>
                             <HeartIcon />
                         </button>
-                        <button className="save_btn">
+                        <button className="save_btn" ref={saveRef}>
                             <SaveIcon isFilled={false} />
                         </button>
                     </div>
