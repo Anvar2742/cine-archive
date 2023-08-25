5841f2ba41cb6cdd5248146cce66fb9a8f344ca9
Anvar <anvarmusa12@gmail.com>
2023-08-25 10:53:58 +0200

    show favorites

--- client/src/hooks/api/useGetApiData.jsx
+++ client/src/hooks/api/useGetApiData.jsx
@@ -1,10 +1,31 @@
-import { axiosMovies } from "../../api/axios";
+import axios, { axiosMovies } from "../../api/axios";
 import useGetUser from "./useGetUser";
 
 const useGetApiData = (mediaType, listType, page, size = 1280) => {
     const getUser = useGetUser();
     const authToken = import.meta.env.VITE_TMDB_AUTH_TOKEN;
     const IMG_BASE_URL = "https://image.tmdb.org/t/p/";
+
+    const getFavorites = async (sessionId) => {
+        const url = `https://api.themoviedb.org/3/account/16104096/favorite/${mediaType}s?language=en-US&page=1&session_id=${sessionId}&sort_by=created_at.asc`;
+        const config = {
+            headers: {
+                accept: "application/json",
+                Authorization:
+                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOWYzYWFmNGY1ZTk4YzRkMTJlM2VjZTI4NDI4NzFiZiIsInN1YiI6IjYzN2Y4Mjg0MjI5YWUyMTU1NDI2NTEyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lM2ZBMJJoT0GkS4xdQlRcxK0AjR9DBzxYN1t0c9_hyw",
+            },
+        };
+
+        const resp = await axios.get(url, config);
+        // console.log(resp);
+        return resp.data;
+    };
+
+    const getFavIds = (favData) => {
+        const results = favData.results;
+        const favIds = results.map((el) => el.id);
+        return favIds;
+    };
 
     const getMovies = async () => {
         try {
@@ -18,25 +39,21 @@ const useGetApiData = (mediaType, listType, page, size = 1280) => {
                 }
             );
             const user = await getUser();
-            const favoriteTitleIds = user.favoriteTitleIds;
-            const watchlistTitleIds = user.watchlistTitleIds;
-            console.log(resp.data);
+            const sessionId = await user.session_id;
+            const favorites = await getFavorites(sessionId);
+            const favIds = getFavIds(favorites);
+
             const updatedResults = await resp.data.results.map((el) => {
-                const userFavId = favoriteTitleIds.filter(
-                    (elF) => elF === el.id
-                );
-                const userSavedId = watchlistTitleIds.filter(
-                    (elF) => elF === el.id
-                );
+                const userFavId = favIds.filter((elF) => elF === el.id);
 
                 return {
                     ...el,
                     backdrop_path: IMG_BASE_URL + `w${size}` + el.backdrop_path,
                     poster_path: IMG_BASE_URL + `w${size}` + el.poster_path,
                     isFav: userFavId.length ? true : false,
-                    isSaved: userSavedId.length ? true : false,
                 };
             });
+            console.log(resp.data);
 
             return updatedResults;
         } catch (error) {
