4a823a41766440365d9368d3051394ebcf141e02
Anvar <anvarmusa12@gmail.com>
2023-08-28 10:04:23 +0200

    genres added to single page

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -62,7 +62,21 @@ const Single = () => {
                     <div className=" py-10">
                         <h1 className=" text-2xl font-bold">{title?.title}</h1>
                         <p className="mt-4">{title?.overview}</p>
-                        <div></div>
+                        <div className="mt-4">
+                            <h3 className=" text-lg">Genres</h3>
+                            <div className="flex gap-2 mt-2">
+                                {title?.genres.map((genre) => {
+                                    return (
+                                        <div
+                                            key={genre.id}
+                                            className=" bg-sec py-1 px-3 rounded-full"
+                                        >
+                                            {genre.name}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
