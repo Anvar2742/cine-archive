037bdc733c4235303fbeda8982a8f053bb1a43b1
Anvar <anvarmusa12@gmail.com>
2023-08-31 17:47:19 +0200

    mobile menu added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,6 +16,19 @@ export default {
             screens: {
                 phone: "420px",
             },
+            transitionDelay: {
+                long: "1s",
+            },
+            keyframes: {
+                "curtain-down": {
+                    "0%": { transform: "translateY(-120%)" },
+                    "50%": { transform: "translateY(10px)" },
+                    "100%": { transform: "translateY(0)" },
+                },
+            },
+            animation: {
+                "curtain-down": "curtain-down 1000ms ease-in-out 1",
+            },
         },
     },
     plugins: [],
