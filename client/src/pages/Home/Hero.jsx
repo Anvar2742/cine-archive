bb0bd9c5dd440bb2689d0bd581cdab6b414f253d
Anvar <anvarmusa12@gmail.com>
2023-09-02 23:37:58 +0200

    slider fade up, slider on hover opacity

--- client/src/pages/Home/Hero.jsx
+++ client/src/pages/Home/Hero.jsx
@@ -12,14 +12,30 @@ const HomeHero = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [heroBg, setHeroBg] = useState(null);
     const [swiper, setSwiper] = useState(null);
-    const [swiperRef, setSwiperRef] = useState(null);
-    const swiperEl = useRef(null);
+    const [swiperEl, setSwiperEl] = useState(null);
+    const [isHover, setIsHover] = useState(false);
+    const swiperRef = useRef(null);
     const getMovies = useGetApiData();
     const location = useLocation();
 
     const handleMouseEnter = (i) => {
         setHeroBg(() => {
             return titlesArr[i].backdrop_path;
+        });
+        setIsHover(true);
+    };
+
+    const handleMouseLeave = (i) => {
+        setIsHover(false);
+    };
+
+    const updateList = (listType) => {
+        setSwiper(null);
+        setSwiperEl(null);
+        setTitlesEls(null);
+        setIsLoadingSlider(true);
+        getMovies("movie", listType, 1).then((data) => {
+            setTitlesArr(data?.results);
         });
     };
 
@@ -39,12 +55,15 @@ const HomeHero = () => {
                 return titlesArr.map((el, i) => {
                     return (
                         <swiper-slide
-                            class="transition-all rounded-md overflow-hidden hover:scale-150 hover:-translate-y-[25%] hover:z-50 group relative"
+                            class={`transition-all rounded-md overflow-hidden hover:scale-[2] hover:-translate-y-[50%] hover:z-50 group relative ${
+                                isHover ? "opacity-50" : ""
+                            } hover:opacity-100`}
                             key={el?.id}
                         >
                             <Link
                                 to={`/titles/${el?.id}`}
                                 onMouseEnter={() => handleMouseEnter(i)}
+                                onMouseLeave={() => handleMouseLeave(i)}
                             >
                                 <img src={el?.poster_path} alt="" />
                                 {el?.isSeen ? (
@@ -57,7 +76,7 @@ const HomeHero = () => {
                                     ""
                                 )}
                                 <div className="absolute bottom-0 left-0 right-0 mx-auto h-full flex items-end justify-center bg-opacity-50 bg-sliderGrad transition-all group-hover:opacity-100 opacity-0">
-                                    <h3 className="pb-4 text-sm px-4 overflow-hidden text-ellipsis whitespace-nowrap font-semibold transition-all duration-300 translate-y-10 opacity-0 group-hover:translate-y-0 group-hover:opacity-100">
+                                    <h3 className="pb-4 text-[9px] px-4 overflow-hidden text-ellipsis whitespace-nowrap font-medium transition-all duration-300 translate-y-10 opacity-0 group-hover:translate-y-0 group-hover:opacity-100">
                                         {el?.title}
                                     </h3>
                                 </div>
@@ -69,14 +88,16 @@ const HomeHero = () => {
 
             setHeroBg(titlesArr[0].backdrop_path);
         }
-    }, [titlesArr]);
+    }, [titlesArr, isHover]);
 
     useEffect(() => {
         if (titlesEls) {
             setSwiper(
                 <swiper-container
-                    class={`w-full overflow-visible transition-opacity ${
-                        isLoadingSlider ? "opacity-0 pointer-events-none" : ""
+                    class={`w-full overflow-visible transition-all duration-700 ${
+                        isLoadingSlider
+                            ? "opacity-0 pointer-events-none translate-y-[200%]"
+                            : "translate-y-0"
                     }`}
                     loop="true"
                     centered-slides="true"
@@ -84,26 +105,26 @@ const HomeHero = () => {
                     space-between="30"
                     init="false"
                     ref={(el) => {
-                        setSwiperRef(el);
+                        setSwiperEl(el);
                     }}
                 >
                     {titlesEls}
                 </swiper-container>
             );
         }
-    }, [titlesEls]);
+    }, [titlesEls, isLoadingSlider]);
 
     // Swiper init
     useEffect(() => {
-        if (swiperRef) {
-            swiperEl.current = swiperRef;
-            // console.log(swiperEl);
+        if (swiperEl) {
+            swiperRef.current = swiperEl;
+            // console.log(swiperRef);
             const params = {
                 injectStyles: [
                     `
-                    .swiper {
-                        overflow: unset;
-                    }
+                        .swiper {
+                            overflow: unset;
+                        }
                     `,
                 ],
                 on: {
@@ -113,11 +134,11 @@ const HomeHero = () => {
                 },
             };
 
-            Object.assign(swiperEl.current, params);
+            Object.assign(swiperRef.current, params);
 
-            swiperEl.current.initialize();
+            swiperRef.current.initialize();
         }
-    }, [swiperRef]);
+    }, [swiperEl]);
 
     register();
 
@@ -126,10 +147,24 @@ const HomeHero = () => {
     return (
         <div
             style={{ "--image-url": `url(${heroBg})` }}
-            className={`h-screen transition-all bg-center bg-cover relative after:block after:w-full after:h-full after:absolute after:top-0 after:left-0 after:bg-primary after:bg-opacity-40 ${
+            className={`h-screen transition-all bg-center bg-cover bg-no-repeat relative after:block after:w-full after:h-full after:absolute after:top-0 after:left-0 after:bg-primary after:bg-opacity-40 ${
                 heroBg ? `bg-[image:var(--image-url)]` : "bg-hero"
             }`}
         >
+            <div className="absolute left-[5%] top-1/3 z-10">
+                <button
+                    onClick={() => updateList("now_playing")}
+                    className="block"
+                >
+                    Now playing
+                </button>
+                <button
+                    onClick={() => updateList("upcoming")}
+                    className="block"
+                >
+                    Upcoming
+                </button>
+            </div>
             <div className="flex items-end justify-center h-full overflow-hidden pb-10 relative">
                 {swiper ? swiper : ""}
             </div>
