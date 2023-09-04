ffb7f8d69d9684b2efc3a94a6c28410589ad2a05
Anvar <anvarmusa12@gmail.com>
2023-09-04 10:19:06 +0200

    search bar styled

--- client/src/components/SearchModal.jsx
+++ client/src/components/SearchModal.jsx
@@ -1,8 +1,19 @@
+import { SearchIcon } from "./svgIcons";
 
 const SearchModal = () => {
-  return (
-    <div className="fixed top-20 left-0 right-0 mx-auto">SearchModal</div>
-  )
-}
+    return (
+        <div className="fixed top-0 left-0 bg-primary bg-opacity-50 w-screen h-screen z-50 pt-20 flex items-start justify-center">
+            <form className=" max-w-xs flex bg-white overflow-hidden rounded-full pr-1 h-8">
+                <input type="text" className="p-2 text-primary" placeholder="Movie title"/>
+                <button type="submit" className=" border-l border-primary h-full px-2 group">
+                    <SearchIcon className="fill-primary w-5 h-5 transition-colors group-hover:fill-sec" />
+                </button>
+            </form>
+            <div className="">
 
-export default SearchModal
+            </div>
+        </div>
+    );
+};
+
+export default SearchModal;
